f65ae755a221d2604d744ccd6effc4e81bd5e53e
Ryo_Odo <marukae@gmail.com>
2021-10-13 01:46:35 +0000

    編集ページ作成

--- app/controllers/chunters_controller.rb
+++ app/controllers/chunters_controller.rb
@@ -1,11 +1,19 @@
 class ChuntersController < ApplicationController
   def index
+    @chunters = Chunter.all
   end
   def new
     @chunter = Chunter.new
   end
   def create
-    Chunter.create(content: params[:chunter][:content])
+    Chunter.create(chunter_params)
     redirect_to new_chunter_path
+  end
+  def show
+    @chunter = Chunter.find(params[:id])
+  end
+  private
+  def chunter_params
+    params.require(:chunter).permit(:content)
   end
 end
